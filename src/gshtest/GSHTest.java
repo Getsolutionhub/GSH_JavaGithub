25348fdc140ef17f7bd77f0e86bb6d27df26edc4
Mayank <mayankkanakiya182003@gmail.com>
2022-06-09 08:48:05 +0530

    test commit - 1

--- src/gshtest/GSHTest.java
+++ src/gshtest/GSHTest.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gshtest;
 
-/**
- *
- * @author Dell
- */
 public class GSHTest {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        This is test - 1
     }
 
 }
